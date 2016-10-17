d76f3372b7c47ac7b81d0b5b6adc27033972ac50
Jonathan Wade <jwade005@seattlecentral.edu>
2016-10-17 16:34:41 -0700

    Update to apache_git_html.sh :permissions
    Added commands to adjust permission levels and setenforce for index.html

--- apache_git_html.sh
+++ apache_git_html.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-echo "Installing Apache server"
+echo "Installing Apache server..."
 sudo yum -y install httpd
 
-echo "Starting HTTP service"
+echo "Starting HTTP service..."
 sudo systemctl enable httpd.service
 
-echo "Starting Apache Server"
+echo "Starting Apache Server..."
 sudo systemctl start httpd.service
 
-echo "Cloning jwade005's github"
+echo "Cloning jwade005's github..."
 sudo yum -y install git
 sudo git clone https://github.com/jwade005/install_scripts.git
 
 echo "Publishing Website..."
 sudo cp install_scripts/index.html /var/www/html
+
+echo "Adjusting Permissions..."
+sudo chmod 644 /var/www/html/index.html
+sudo setenforce 0
