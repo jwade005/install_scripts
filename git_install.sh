c33b22155df91742f20c52f98f524642965f9d12
Jonathan Wade <jwade005@seattlecentral.edu>
2016-10-16 19:39:32 -0700

    Update and rename git_install to git_install.sh

--- git_install.sh
+++ git_install.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 sudo yum -y install git
-git clone https://github.com/jwade005/
+git clone https://github.com/jwade005/install_scripts.git
+git clone https://github.com/nic-instruction/NTI-300/
 
-echo "a clone of the jwade005 repository is now sitting in this dir, along with a copy of this script \n"
+echo "a clone of the jwade005 repository and NTI-300 repository is now sitting in this dir, along with a copy of this script \n"
 echo "For a git command line cheet sheet check out https://services.github.com/kit/downloads/github-git-cheat-sheet.pdf \n"
 echo "Basic commands:
       git pull            # will get you repo updates
